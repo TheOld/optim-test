a0f31e312412a0e528db9ba2a92aa7d467c8a8d9
Leandro Rodrigues <leandro@thankyoupayroll.co.nz>
2020-12-09 16:43:56 +1300

    Using the extracted tags to find the correspondent values of each

--- routes/parse-content.js
+++ routes/parse-content.js
@@ -13,6 +13,25 @@ router.post('/', function (req, res) {
     const { content } = req.body;
     // First we'll extract the XML tags from the string (includes closing ones)
     const tags = extractXMLTags({ xml: content });
+
+    // Once we obtain a list of tags, we'll query the string for the values matching the tags on the list
+    const values = {};
+
+    for (let index = 0; index < tags.length; index++) {
+      const tag = tags[index];
+
+      if (!tag.includes('/')) {
+        // Let's strip the tag from it's brackets
+        const strippedTag = tag.replace(/\W+/g, '');
+        // Here we're manually building a closing tag based on the current tag in the list - not used in validation for missing closing tag though
+        const closingTag = `</${tag.substring(1)}`;
+
+        // Now we get the value from inside the valid tags, if the tag is empty we'll set the valu to `null` and treat it accordingly
+        const value = content.split(tag).pop().split(closingTag)[0] || null;
+
+      }
+    }
+
   } catch (error) {
     // Print error message on the console
     console.error(`file: content-parser.js ~ line 12 ~ error`, error);
